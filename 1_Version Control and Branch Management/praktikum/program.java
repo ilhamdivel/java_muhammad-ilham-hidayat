f6e25b658200a60c0f9f2ac6527b9341a6992368
Ilham <muhammadilhamhidayat3@gmail.com>
2022-02-22 13:55:04 +0700

    menmabah nama

--- 1_Version Control and Branch Management/praktikum/program.java
+++ 1_Version Control and Branch Management/praktikum/program.java
@@ -2,5 +2,8 @@ package praktikum;
 public class program {
   public static void main(String[] args) {
       System.out.println("Hello, World!"); 
+      System.out.println("Hello, Ilham"); 
+      System.out.println("Hello, Hidayat"); 
+      System.out.println("Hello, Alterra!"); 
   }
 }
