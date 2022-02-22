a0618c1c7d3656951dec9dc8695257188c19d608
Ilham <muhammadilhamhidayat3@gmail.com>
2022-02-22 13:33:51 +0700

    Menambah featureB

--- 1_Version Control and Branch Management/praktikum/program.java
+++ 1_Version Control and Branch Management/praktikum/program.java
@@ -1,10 +1,6 @@
 package praktikum;
 public class program {
   public static void main(String[] args) {
-<<<<<<< HEAD
-      System.out.println("Hello  World! FeatureB"); 
-=======
-      System.out.println("Hello, World! Feature A"); 
->>>>>>> featureA
+      System.out.println("Hello, World! FeatureB"); 
   }
 }
