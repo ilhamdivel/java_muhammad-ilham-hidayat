437c5314ad9802b0fce1dfd3055b7032d1b4cc1b
Ilham <muhammadilhamhidayat3@gmail.com>
2022-02-22 13:51:45 +0700

    menambahkan featureB

--- 1_Version Control and Branch Management/praktikum/program.java
+++ 1_Version Control and Branch Management/praktikum/program.java
@@ -1,6 +1,7 @@
 package praktikum;
 public class program {
   public static void main(String[] args) {
-      System.out.println("Hello, World! FeatureB"); 
+      System.out.println("Hello, World! FeatureA");
+      System.out.println("Hello, World! FeatureB");  
   }
 }
